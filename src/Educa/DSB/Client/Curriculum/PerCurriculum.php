332cfc2e5372cda6180993419dae96e13a6e79a0
Wouter Admiraal <wad@wadmiraal.net>
2016-07-07 16:45:06 +0200

    Revert previous commit

--- src/Educa/DSB/Client/Curriculum/PerCurriculum.php
+++ src/Educa/DSB/Client/Curriculum/PerCurriculum.php
@@ -311,6 +311,7 @@ class PerCurriculum extends BaseCurriculum
         // The reason we put that stupid "?" there is to simplify unit tests.
         $objectives = json_decode(@file_get_contents("$url/objectifs?"), true);
         $themes = array();
+        $domains = array();
         $disciplines = array();
 
         // Prepare a little function for reducing school year lists.
@@ -363,15 +364,25 @@ class PerCurriculum extends BaseCurriculum
                 // What is the domain ID?
                 $domainId = $objectiveData['domaine']['id'];
 
-                $domain = new PerTerm('domaines', $domainId, array(
-                    'fr' => $objectiveData['domaine']['nom'],
-                ));
-                $cycle->addChild($domain);
+                // Contrary to objectives and progressions, domains are shared.
+                // Check if we already created this domain. If we did, re-use
+                // the old one.
+                if (isset($domains[$cycleNum][$domainId])) {
+                    $domain = $domains[$cycleNum][$domainId];
+                } else {
+                    $domain = new PerTerm('domaines', $domainId, array(
+                        'fr' => $objectiveData['domaine']['nom'],
+                    ));
+                    $cycle->addChild($domain);
 
-                // Store the description in the dictionary.
-                $description = $domain->describe();
-                unset($description->id);
-                $dictionary["domaines:{$domainId}"] = $description;
+                    // Store it.
+                    $domains[$cycleNum][$domainId] = $domain;
+
+                    // Store the description in the dictionary.
+                    $description = $domain->describe();
+                    unset($description->id);
+                    $dictionary["domaines:{$domainId}"] = $description;
+                }
 
                 // Prepare all theme names.
                 foreach ($objectiveData['thematiques'] as $themeData) {
